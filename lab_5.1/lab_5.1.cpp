915fba91be27d8fb96bf6b228a355a0eb712cccc
bozhenalesivik2025-gif <bozhena.lesiv.ik.2025@lpnu.ua>
2025-10-15 21:47:26 +0300

    first master commit

---
+++ lab_5.1/lab_5.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+double f(const double x, const double y);
+
+int main()
+{
+    double s, t;
+
+    cout << " s: ";
+    cin >> s;
+    cout << " t: ";
+    cin >> t;
+
+    double z = (f(t, s) + f(s, 1)) / (1 + pow(f(t, t * s), 2));
+    cout << "result z = " << z << endl;
+
+    return 0;
+}
+double f(const double x, const double y)
+{
+    return (x * x - cos(y) + y * y) / (1 + x * y);
+}
