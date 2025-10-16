a92567b0b94b953ee72fd1baf9ab8f1ccfcbcadc
bozhenalesivik2025-gif <bozhena.lesiv.ik.2025@lpnu.ua>
2025-10-16 18:05:41 +0300

    second test

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.1/lab_5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double z = f(25, 1);
+			Assert::AreEqual(24.05, z, 0.01);
+
+		}
+	};
+	
+}
