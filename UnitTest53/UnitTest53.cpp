c95675083f2f2a8407b1861ab601a10b4292d914
bozhenalesivik2025-gif <bozhena.lesiv.ik.2025@lpnu.ua>
2025-10-17 16:09:49 +0300

    lab_5.3

---
+++ UnitTest53/UnitTest53.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.3/lab_5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+    TEST_CLASS(FunctionSTests) 
+    {
+    public:
+
+      
+        TEST_METHOD(TestMethod_S_AbsMoreThanOne)
+        {
+            double x = 2.0;
+            double expected = 0.158775;
+
+            double actual = s(x);
+
+            Assert::AreEqual(expected, actual, 0.0001);
+        }
+
+        TEST_METHOD(TestMethod_S_AbsLessOrEqualOne_Positive)
+        {
+            double x = 0.5;
+            double expected = 0.521095;
+
+            double actual = s(x);
+
+            Assert::AreEqual(expected, actual, 0.0001);
+        }
+
+        TEST_METHOD(TestMethod_S_AbsLessOrEqualOne_Negative)
+        {
+            double x = -0.5;
+            double expected = -0.521095;
+
+            double actual = s(x);
+
+            Assert::AreEqual(expected, actual, 0.0001);
+        }
+        TEST_METHOD(TestMethod_S_BoundaryCase_One)
+        {
+        
+            double x = 1.0;
+            double expected = 1.17520;
+
+            double actual = s(x);
+
+            Assert::AreEqual(expected, actual, 0.0001);
+        }
+    };
+}
---
+++ lab_5.3/lab_5.3.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+double s(const double x);
+
+int main()
+{
+    double tp, tk, Y;
+    int n;
+
+    cout << "tp = "; cin >> tp; 
+    cout << "tk = "; cin >> tk; 
+    cout << "n = "; cin >> n;   
+ 
+    double ts = (tk - tp) / n;
+    double t = tp;
+
+    cout << "---------------------------------" << endl;
+    cout << "|" << setw(10) << "t" << setw(5) << "|" << setw(15) << "Y" << setw(5) << "|" << endl;
+    cout << "---------------------------------" << endl;
+
+    while (t <= tk)
+    {
+        Y = s(t * t + 1) + 2 * pow(s(1 - t), 2) + s(1);
+
+        cout << "|" << fixed << setprecision(4) << setw(10) << t << setw(5) << "|"
+            << fixed << setprecision(6) << setw(15) << Y << setw(5) << "|" << endl;
+        t += ts;
+    }
+
+    cout << "---------------------------------" << endl;
+    return 0;
+}
+double s(const double x)
+{
+    if (abs(x) > 1)
+    {
+        return (cos(x) * cos(x) + 1) / exp(x);
+    }
+    else
+    {
+        double S = 0;
+        int k = 0;
+
+        double a = x;
+        S = a;
+
+        do
+        {
+            k++;
+           
+            double R = (x * x) / ((2.0 * k) * (2.0 * k + 1.0));
+            a *= R;
+            S += a;
+        } while (k < 4); 
+
+        return S;
+    }
+}
